0782bc3fd9b4c50d6e432f68bdb1825d294085bc
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 14:23:20 +0300

    Fix 9-s sum Problem solved: now calculates the sum correctly

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,12 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int lastNumber = 0;
         int sum = 0;
         for (int i = 1; i <= lengthOfLastNumber; i++) {
             if (i == 1) {
                 sum += 9;
+                lastNumber = sum;
             } else {
-                sum += 9 * (int) Math.pow(10, i - 1) + sum;
+                lastNumber = lastNumber + (9 * (int) Math.pow(10, i - 1));;
+                sum += lastNumber;
             }
         }
         System.out.println(sum);
