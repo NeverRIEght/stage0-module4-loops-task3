3b25e8bade30ca917f2fd57f112bb74a783244ec
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 13:31:06 +0300

    Digits sum

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int t) {
+        int digitsCount = ("" + t).length();
+        int sum = 0;
 
+        for (int i = 1; i <= digitsCount; i++) {
+            int divider = (int)Math.pow(10, i);
+            if(i == 1) {
+                sum += t % divider;
+            } else {
+                sum += ((t % divider) - (t % (divider / 10))) / (divider / 10);
+            }
+        }
+        System.out.println(sum);
     }
 }
