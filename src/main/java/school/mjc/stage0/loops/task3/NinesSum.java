83f846f35ec69b1b8c3074982041f96ac56d6edb
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 13:44:37 +0300

    9-s sum

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            if (i == 1) {
+                sum += 9;
+            } else {
+                sum += 9 * (int) Math.pow(10, i - 1);
+            }
+        }
+        System.out.println(sum);
     }
 }
