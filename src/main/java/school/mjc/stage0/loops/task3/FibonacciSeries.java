7b09dc84bf0cdd3b9619d90f825d64a53f513b1a
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 13:59:08 +0300

    Fibonacci

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int preLastNumber = 0;
+        int lastNumber = 1;
+        for (int i = 0; i < lastFibonacci; i++) {
+            if(i == 0) {
+                System.out.println(0);
+            } else if(i == 1) {
+                System.out.println(1);
+            } else {
+                int newNumber = lastNumber + preLastNumber;
+                System.out.println(newNumber);
+                preLastNumber = lastNumber;
+                lastNumber = newNumber;
+            }
+        }
     }
 }
