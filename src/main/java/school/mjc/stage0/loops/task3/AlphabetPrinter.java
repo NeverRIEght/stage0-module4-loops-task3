6125e876566fc26334761e99e95478ca79b2b292
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 11:34:16 +0300

    Alphabet

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        //English symbols in ASCII are from 97 to 122
+        for (int i = 97; i < 97 + 26; i++) {
+            System.out.println((char) i);
+        }
     }
 }
